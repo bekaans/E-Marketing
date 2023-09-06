dc3d9c7419cda14266408ee6c0eac79449b6db69
Berke Kaan <bekaans@icloud.com>
2023-09-06 22:25:55 +0300

    Add DesignTimeContextFactory => .Persistence/

---
+++ Infrastructure/ASP.NetCore-AngularWeb.Persistence/DesignTimeContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using ASP.NetCore_AngularWeb.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NetCore_AngularWeb.Persistence
+{
+    public class DesignTimeContextFactory : IDesignTimeDbContextFactory<EmarketingContextDb_>
+    {
+        public EmarketingContextDb_ CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<EmarketingContextDb_> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseSqlServer(Configurations.connectingString);
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
