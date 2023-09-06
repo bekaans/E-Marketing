9e47f8a1b9cead54df647647533495b5f0513a12
Berke Kaan <bekaans@icloud.com>
2023-09-06 22:26:51 +0300

    Add Configuration => .Persistence/

---
+++ Infrastructure/ASP.NetCore-AngularWeb.Persistence/Configurations.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NetCore_AngularWeb.Persistence
+{
+     static class Configurations
+    {
+        static public string connectingString
+        {
+            get
+            {
+                ConfigurationManager configurationManager = new();
+                configurationManager.SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../../Presentation/ASP.NetCore-AngularWeb.API"));
+                configurationManager.AddJsonFile("appsettings.json");
+
+                return configurationManager.GetConnectionString("SQL");
+            }
+        }
+    }
+}
