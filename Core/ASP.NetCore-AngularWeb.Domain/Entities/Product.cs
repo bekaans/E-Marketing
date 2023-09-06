1c076b2f3617f2d4fafda20e547db2a8c021eaeb
Berke Kaan <bekaans@icloud.com>
2023-09-06 14:59:37 +0300

    Add Product => .Domain/Entities

---
+++ Core/ASP.NetCore-AngularWeb.Domain/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using ASP.NetCore_AngularWeb.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NetCore_AngularWeb.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public long Price { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
