18003f5fdb6de281327fe6ea27da7da005518702
Berke Kaan <bekaans@icloud.com>
2023-09-06 15:37:52 +0300

    Add EmarketingContextDb => .Persistence/Context/

---
+++ Infrastructure/ASP.NetCore-AngularWeb.Persistence/Contexts/EmarketingContextDb,.cs
@@ -0,0 +1,24 @@
+﻿using ASP.NetCore_AngularWeb.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NetCore_AngularWeb.Persistence.Contexts
+{
+    public class EmarketingContextDb_ : DbContext
+    {
+        public EmarketingContextDb_(DbContextOptions options) : base(options) 
+        {
+
+        }
+
+      
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
