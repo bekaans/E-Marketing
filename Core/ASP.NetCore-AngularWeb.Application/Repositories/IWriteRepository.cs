149a2a0212d6b1b2e99f6787fa9dd661d00d9521
Berke Kaan <bekaans@icloud.com>
2023-09-06 23:39:47 +0300

    Update IWriteRepository , IReadRepository , IRepository

--- Core/ASP.NetCore-AngularWeb.Application/Repositories/IWriteRepository.cs
+++ Core/ASP.NetCore-AngularWeb.Application/Repositories/IWriteRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ASP.NetCore_AngularWeb.Domain.Entities.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,13 @@ using System.Threading.Tasks;
 
 namespace ASP.NetCore_AngularWeb.Application.Repositories
 {
-    public interface IWriteRepository<T> : IRepository<T> where T : class
+    public interface IWriteRepository<T> : IRepository<T> where T : BaseEntity
     {
         Task<bool> AddAsync(T model);
         Task<bool> AddAsync(List<T> model);
         Task<bool> Remove(T model);
         Task<bool> Remove(string id);
         Task<bool> Update(T model);
+        Task<int> Save();
     }
 }
