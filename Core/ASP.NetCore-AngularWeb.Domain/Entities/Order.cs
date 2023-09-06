6253e75fe1315a4a7a7f0830a874b89ba469b049
Berke Kaan <bekaans@icloud.com>
2023-09-06 14:59:07 +0300

    Add Order => .Domain/Entities

---
+++ Core/ASP.NetCore-AngularWeb.Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using ASP.NetCore_AngularWeb.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NetCore_AngularWeb.Domain.Entities
+{
+    public class Order : BaseEntity
+    {
+        public string Description { get; set; }
+        public string Adress { get; set; }
+        public ICollection<Product> Products { get; set; }
+        public int CustomerID { get; set; }
+        public Customer customer { get; set; }
+    }
+}
