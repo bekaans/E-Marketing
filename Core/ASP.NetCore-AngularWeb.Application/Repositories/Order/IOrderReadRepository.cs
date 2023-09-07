143b97acc528753bbbc1ce86ee3b326a3e1af3a2
Berke Kaan <bekaans@icloud.com>
2023-09-07 12:57:31 +0300

    Add ICustomerReadRepository,ICustomerWriteRepository,IOrderReadRepository,IOrderWriteRepository,IProductReadRepository,IProductWriteRepository => .Application/Repositories/

---
+++ Core/ASP.NetCore-AngularWeb.Application/Repositories/Order/IOrderReadRepository.cs
@@ -0,0 +1,13 @@
+﻿using ASP.NetCore_AngularWeb.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NetCore_AngularWeb.Application.Repositories
+{
+    public interface IOrderReadRepository : IRepository<Order>
+    {
+    }
+}
