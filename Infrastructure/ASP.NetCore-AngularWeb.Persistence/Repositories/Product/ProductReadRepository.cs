2eb9569271d7e8d1ab63b8b881a24b546a5d4f84
Berke Kaan <bekaans@icloud.com>
2023-09-07 12:58:33 +0300

    Add CustomerReadRepository,CustomerWriteRepository,OrderReadRepository,OrderWriteRepository,ProductReadRepository,ProductWriteRepository => .Persistence/Repositories/

---
+++ Infrastructure/ASP.NetCore-AngularWeb.Persistence/Repositories/Product/ProductReadRepository.cs
@@ -0,0 +1,18 @@
+﻿using ASP.NetCore_AngularWeb.Application.Repositories;
+using ASP.NetCore_AngularWeb.Domain.Entities;
+using ASP.NetCore_AngularWeb.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP.NetCore_AngularWeb.Persistence.Repositories
+{
+    public class ProductReadRepository : ReadRepository<Product>, IProductReadRepository
+    {
+        public ProductReadRepository(EmarketingContextDb_ context) : base(context)
+        {
+        }
+    }
+}
