1c66609c8b1af125f4968a6ab8c5c06d7a496171
Berke Kaan <bekaans@icloud.com>
2023-09-07 12:58:51 +0300

    Update ServiceRegistration => .Persistence

--- Infrastructure/ASP.NetCore-AngularWeb.Persistence/ServiceRegistration.cs
+++ Infrastructure/ASP.NetCore-AngularWeb.Persistence/ServiceRegistration.cs
@@ -1,4 +1,6 @@
-﻿using ASP.NetCore_AngularWeb.Persistence.Contexts;
+﻿using ASP.NetCore_AngularWeb.Application.Repositories;
+using ASP.NetCore_AngularWeb.Persistence.Contexts;
+using ASP.NetCore_AngularWeb.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,7 +19,13 @@ namespace ASP.NetCore_AngularWeb.Persistence
         
         public static void AddPersistenceRegistration(this IServiceCollection services)
         {
-            services.AddDbContext<EmarketingContextDb_>(options => options.UseSqlServer(Configurations.connectingString));
+            services.AddDbContext<EmarketingContextDb_>(options => options.UseSqlServer(Configurations.connectingString),ServiceLifetime.Singleton);
+            services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository,OrderWriteRepository>();
         }
     }
 }
