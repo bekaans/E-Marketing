3856ae174e508b733932e7949816e038ba2f43b6
Berke Kaan <bekaans@icloud.com>
2023-09-23 22:22:24 +0300

    install npm ngx-spinner

--- EmarketingClient/src/app/admin/components/orders/orders.component.ts
+++ EmarketingClient/src/app/admin/components/orders/orders.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.scss']
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
+  constructor(private spinner:NgxSpinnerService){}
+  ngOnInit(): void {
+    this.spinner.show();
+    setTimeout(()=>{
+      this.spinner.hide();
+    },500);
+  }
 
 }
