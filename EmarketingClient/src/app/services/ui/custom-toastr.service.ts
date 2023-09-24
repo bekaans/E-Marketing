e8ca36bd68ef32a4e68d7feab39eb398725971fc
Berke Kaan <bekaans@icloud.com>
2023-09-24 23:23:14 +0300

    - alertify

--- EmarketingClient/src/app/services/ui/custom-toastr.service.ts
+++ EmarketingClient/src/app/services/ui/custom-toastr.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {ToastrService} from 'ngx-toastr';
-import { MessageType } from '../admin/alertify.service';
+
 
 @Injectable({
   providedIn: 'root'
