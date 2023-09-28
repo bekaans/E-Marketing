e65453155d5215cea2f5f99beb7292beb4c2ccb0
Berke Kaan <bekaans@icloud.com>
2023-09-28 20:42:43 +0300

    Add FluentValidationNuget

--- Presentation/ASP.NetCore-AngularWeb.API/Program.cs
+++ Presentation/ASP.NetCore-AngularWeb.API/Program.cs
@@ -1,15 +1,19 @@
 
 
+using ASP.NetCore_AngularWeb.Application.Validations.Products;
+using ASP.NetCore_AngularWeb.Infrastructure.Filters;
 using ASP.NetCore_AngularWeb.Persistence;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpsPolicy;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddPersistenceRegistration();
-builder.Services.AddControllers();
+builder.Services.AddControllers(options=>options.Filters.Add<ValidationFilter>()).AddFluentValidation(configuration=>configuration.RegisterValidatorsFromAssemblyContaining<CreateProductValidator>())
+    .ConfigureApiBehaviorOptions(options=>options.SuppressModelStateInvalidFilter=true);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod()));
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.WithOrigins("http://localhost:4200", "http://localhost:4200").AllowAnyHeader().AllowAnyMethod()));
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
