6ec1ab3ee7b8e51221ec72e3afe3f38ca67029d7
Berke Kaan <bekaans@icloud.com>
2023-09-28 20:44:12 +0300

    Add Validations

--- Presentation/ASP.NetCore-AngularWeb.API/Controllers/ProductsController.cs
+++ Presentation/ASP.NetCore-AngularWeb.API/Controllers/ProductsController.cs
@@ -37,6 +37,10 @@ namespace ASP.NetCore_AngularWeb.API.Controllers
         [HttpPost]
         public async Task<IActionResult> Post(VM_Create_Product model)
         {
+            if (ModelState.IsValid)
+            {
+
+            }
             await _productWriteRepository.AddAsync(new()
             {
                 Name = model.Name,
