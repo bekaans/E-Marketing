1c13691a4d652f5663b5c6f25fcba39d1270d1d2
Berke Kaan <bekaans@icloud.com>
2023-10-01 15:31:16 +0300

    .gitignore

--- EmarketingClient/src/app/admin/components/products/list/list.component.ts
+++ EmarketingClient/src/app/admin/components/products/list/list.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource, _MatTableDataSource } from '@angular/material/table';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, spinnerType } from 'src/app/base/base.component';
+import { List_Product } from 'src/app/contracts/list_product';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { ProductService } from 'src/app/services/common/models/product.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent extends BaseComponent implements  OnInit {
+
+  constructor(spinner:NgxSpinnerService,private productService:ProductService,private alertfiyservice:AlertifyService){
+    super(spinner);
+  }
+  displayedColumns:string[]=['Name','Stock','Price','CreatedDate','UpdatedDate'];
+  dataSource:MatTableDataSource<List_Product>=null;
+  @ViewChild(MatPaginator) paginator:MatPaginator;
+  async ngOnInit() {
+    this.showSpinner(spinnerType.ballSpinFadeRotating);
+   const allProducts : List_Product[]= await this.productService.read(()=>this.hideSpinner(spinnerType.ballSpinFadeRotating),errorMessage=> this.alertfiyservice.message(errorMessage,{
+      delay:2,
+      messageType:MessageType.Error,
+      position:Position.TopRight
+    }))
+    this.dataSource = new MatTableDataSource<List_Product>(allProducts);
+  }
 
 }
--- EmarketingClient/src/app/services/common/models/product.service.ts
+++ EmarketingClient/src/app/services/common/models/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClientService } from '../http-client.service';
 import { Create_Product } from 'src/app/contracts/create_product';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { List_Product } from 'src/app/contracts/list_product';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +21,14 @@ export class ProductService {
       (errorResponse: HttpErrorResponse)=>{
         const error=errorResponse.error;
       });
+
+
+  }
+  async read(succesCallBack?:()=>void,errorCallBack?:(errorMessage : string)=>void):Promise<List_Product[]>{
+   const promiseData : Promise<List_Product[]> = this.httpClientService.get<List_Product[]>({
+      controller:"products"
+    }).toPromise();
+    promiseData.then(d=>succesCallBack()).catch((errorResponse:HttpErrorResponse)=>errorCallBack(errorResponse.message))
+    return await promiseData;
   }
 }
