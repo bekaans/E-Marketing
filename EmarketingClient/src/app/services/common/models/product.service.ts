3802a7cf1ffa59dd09285357c7501f2ee8e99ae4
Berke Kaan <bekaans@icloud.com>
2023-10-07 19:53:43 +0300

    Add List Methods => services/common/models/product/

--- EmarketingClient/src/app/services/common/models/product.service.ts
+++ EmarketingClient/src/app/services/common/models/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClientService } from '../http-client.service';
 import { Create_Product } from 'src/app/contracts/create_product';
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { List_Product } from 'src/app/contracts/list_product';
 
 
 @Injectable({
@@ -11,7 +12,7 @@ export class ProductService {
 
   constructor(private httpClientService : HttpClientService) { }
 
-  createProduct(product:Create_Product,successCallback?:any,errorCallBack?:any){
+  createProduct(product:Create_Product,successCallback?: () => void,errorCallBack?:(errorMessage:string)=>void){
     this.httpClientService.post({
       controller:"products"
     },product).subscribe(result=>{
@@ -28,5 +29,13 @@ export class ProductService {
         errorCallBack(message);
     });
   }
- 
+ async readProduct(page:number=0,size:number=5,successCallback?: ()=>void,errorCallBack?: (errorMessage:string)=>void):Promise<List_Product[]>{
+  const promiseData:Promise<List_Product[]> =  this.httpClientService.get<List_Product[]>({
+    controller:"products"
+  }).toPromise();
+    promiseData.then(d=>successCallback())
+    .catch((errorResponse:HttpErrorResponse)=>errorCallBack(errorResponse.message))
+
+     return await promiseData;
+ }
 }
